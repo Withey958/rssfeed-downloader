e330bb0e1d7ff8bf4657f95df864c95b86e4c714
luke withey <lukewithey@lukes-MacBook-Pro.local>
2020-04-22 09:40:44 +0100

    adapt to use view model

--- app/src/main/java/com/example/top10downloader/FeedAdapter.kt
+++ app/src/main/java/com/example/top10downloader/FeedAdapter.kt
@@ -17,13 +17,18 @@ class ViewHolder(v: View) {
 class FeedAdapter(
     context: Context,
     private val resource: Int,
-    private val applications: List<FeedEntry>
+    private var applications: List<FeedEntry>
 ) : ArrayAdapter<FeedEntry>(context, resource) {
 
     private val inflater = LayoutInflater.from(context)
 
     override fun getCount(): Int {
         return applications.size
+    }
+
+    fun setFeedList(feedList: List<FeedEntry>) {
+        this.applications = feedList
+        notifyDataSetChanged()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
